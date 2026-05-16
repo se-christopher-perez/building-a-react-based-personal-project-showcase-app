bd49b256945313a71ce010d82365af5a5dd83b07
Christopher Perez <Data.Christopher.Perez@Gmail.com>
2026-05-16 04:30:01 -0400

    Added useRef on page load on searchbar

--- coffee-r-us/src/components/SearchBar.jsx
+++ coffee-r-us/src/components/SearchBar.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 function SearchBar({ search, setSearch }) {
 
+    const inputRef = useRef(null)
+
+    useEffect(() => {
+
+        inputRef.current.focus()
+
+    }, [])
 
     return (
         <>
             <div className="search-container">
 
                 <label htmlFor="search-input"></label>
-                <input id="search-input" type="text" placeholder='Search...' value={search} onChange={(e) => setSearch(e.target.value)} />
+                <input id="search-input" ref={inputRef} type="text" placeholder='Search...' value={search} onChange={(e) => setSearch(e.target.value)} />
 
             </div>
         </>
